04c78dd80a989d8f89bc3e65a0e253eec5bfbb0e
Prabhav49 <iamprabhav23@gmail.com>
2024-11-03 14:40:08 +0530

    Added Encrypt Password

--- src/main/java/com/prabhav/yummy/auth/AuthService.java
+++ src/main/java/com/prabhav/yummy/auth/AuthService.java
@@ -3,6 +3,7 @@ package com.prabhav.yummy.auth;
 import com.prabhav.yummy.entity.Customer;
 import com.prabhav.yummy.repo.CustomerRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,14 +11,15 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final CustomerRepo customerRepo;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public String login(String email, String password) {
         Customer customer = customerRepo.findByEmail(email);
 
-        if (customer != null && customer.getPassword().equals(password)) {
+        if (customer != null && passwordEncoder.matches(password, customer.getPassword())) {
             return "User logged in successfully.";
         } else {
-            return "Invalid email or password.";
+            return "Invalid username or password.";
         }
     }
 }
--- src/main/java/com/prabhav/yummy/service/CustomerService.java
+++ src/main/java/com/prabhav/yummy/service/CustomerService.java
@@ -1,12 +1,12 @@
 package com.prabhav.yummy.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import com.prabhav.yummy.entity.Customer;
-import com.prabhav.yummy.dto.CustomerResponse;
 import com.prabhav.yummy.dto.CustomerRequest;
 import com.prabhav.yummy.mapper.CustomerMapper;
 import com.prabhav.yummy.repo.CustomerRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +14,12 @@ public class CustomerService {
 
     private final CustomerRepo repo;
     private final CustomerMapper mapper;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public String createCustomer(CustomerRequest request) {
         Customer customer = mapper.toEntity(request);
+        customer.setPassword(passwordEncoder.encode(customer.getPassword()));
+
         repo.save(customer);
         return "Created";
     }
