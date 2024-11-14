c9a6de73ddc87f71524a173f13ee2f5b868363b5
Prabhav49 <iamprabhav23@gmail.com>
2024-11-14 14:46:57 +0530

    Created Product Entity and its CRUD API

---
+++ src/main/java/com/prabhav/yummy/repo/ProductRepo.java
@@ -0,0 +1,12 @@
+package com.prabhav.yummy.repo;
+
+import com.prabhav.yummy.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.price BETWEEN 15 AND 30 ORDER BY p.price ASC ")
+    List<Product> findTop2ProductsInRange();
+}
---
+++ src/main/java/com/prabhav/yummy/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.prabhav.yummy.service;
+
+import com.prabhav.yummy.entity.Product;
+import com.prabhav.yummy.repo.ProductRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepo productRepo;
+
+    public Product createProduct(Product product) {
+        return productRepo.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepo.findById(id).orElse(null);
+    }
+
+    public Product updateProduct(Product product) {
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> getTop2ProductsInRange() {
+        return productRepo.findTop2ProductsInRange();
+    }
+}
